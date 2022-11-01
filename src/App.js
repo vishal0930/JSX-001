9941e0f3c5069c22418b93eb7e2a7ea527c24880
Vishal <43312718+vishal0930@users.noreply.github.com>
2022-11-01 18:24:24 +0530

    Add files via upload
    Assignment 1 (JSX)

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>About Us</h1>
+        <p>Build your dream career in tech</p>
+        <p>Geekster is on a mission to help people build great careers. We believe that quality education should be accessible to everyone without financial or geographical barriers- thus, we offer skill based programs at Zero upfront cost and provide complete placement assistance.</p>
+        <p>Geekster programs are for people who are interested to build their career in tech. You are eligible to apply if you are a final year student, a recent graduate or a working professional.</p>
+        <p>Having a CS only background is not a prerequisite for applying to the program. We encourage all engineering candidates willing to build a career in tech to apply irrespective of their branch background.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
